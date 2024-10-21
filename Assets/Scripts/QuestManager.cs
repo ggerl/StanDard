1e7c22d2bfe49ec2ce52d790899559b6b247148e
ljj <sumkol123@gmail.com>
2024-10-21 12:51:23 +0900

    [Init] 1번문항 완료

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,42 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour
+{
+    // [구현사항 1] 정적 필드 정의
+    private static QuestManager instance;
+
+    // [구현사항 2] 정적 프로퍼티 정의
+    public static QuestManager Instance
+    {
+        get
+        {
+            if(instance == null)
+            {
+                instance = FindObjectOfType<QuestManager>();
+
+                if(instance == null)
+                {
+                    GameObject obj = new  GameObject("QuestManager");
+                    instance = obj.AddComponent<QuestManager>(); 
+                }
+            }
+            
+            return instance;
+        }
+    }
+
+    // [구현사항 3] 인스턴스 검사 로직
+    private void Awake()
+    {
+        if(instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        
+        instance = this;
+    }
+}
